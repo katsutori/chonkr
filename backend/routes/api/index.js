59cc5eb8d9ec540590a79c5204b57ff8f23731a3
Thien Dang <ctap105@gmail.com>
2022-01-06 13:32:23 -0500

    middleware tested

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+const asyncHandler = require('express-async-handler')
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js')
+const { User } = require('../../db/models')
+
+// test middleware
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user)
+// })
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     })
+//     setTokenCookie(res, user)
+//     return res.json({ user })
+// }))
+
+// router.get('/restore-user', restoreUser, (req, res) => {
+//     return res.json(req.user)
+// })
+
 router.get('/', (req,res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken())
     res.send('Hello World')
