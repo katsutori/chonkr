76a9cd5f7bcb918abdf79694fab6c32aff199563
Thien Dang <ctap105@gmail.com>
2022-01-06 12:18:27 -0500

    api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const apiRouter = require('./api')
-
-router.use('/api', apiRouter)
-
-router.get('/hello/world', (req, res) => {
+router.get('/', (req,res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken())
     res.send('Hello World')
+})
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body })
+
 })
 
 module.exports = router
