9f460f9fa1c752d6f5de4d66b9d71c568f751311
Thien Dang <ctap105@gmail.com>
2022-01-07 14:49:01 -0500

    index routes adjusted

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,31 +4,31 @@ const router = express.Router()
 const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
-
 if (process.env.NODE_ENV === 'production') {
-    const path = require('path')
+    const path = require('path');
 
     router.get('/', (req, res) => {
-        res.cookie('XSFR-TOKEN', req.csrfToken())
-        return res.sendFile(
-            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-        )
-    })
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+    });
 
-    router.use(express.static(path.resolve('../frontend/build')))
+    router.use(express.static(path.resolve("../frontend/build")));
 
     router.get(/^(?!\/?api).*/, (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken())
-        return res.sendFile(__dirname, '../../frontend', 'build', 'index.html')
-    })
-}
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+    });
+  }
 
-if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken())
-        return res(201).json({})
-    })
-}
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      res.status(201).json({});
+    });
+  }
 
-
-module.exports = router
+  module.exports = router;
