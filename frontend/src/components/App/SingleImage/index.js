d56f005ff13e7aed7b1bd6a3e597636db6e9afe9
Thien Dang <ctap105@gmail.com>
2022-01-10 09:06:14 -0500

    fixed direct linking to individual photos

--- frontend/src/components/App/SingleImage/index.js
+++ frontend/src/components/App/SingleImage/index.js
@@ -1,19 +1,28 @@
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import * as sessionActions from '../../../store/session'
+
+import { getAllPhotos } from '../../../store/photos'
 import './SingleImage.css'
 
 const PhotoDetail = () => {
+    const dispatch = useDispatch()
     const { id } = useParams()
+    const sessionUser = useSelector(state => state.session.user)
     const all = useSelector(state => state.photoState.entries)
     const single = all.find(one => one.id === +id)
 
+    useEffect(() => {
+        dispatch(getAllPhotos())
+    }, [dispatch])
+
     return (
         <div className='single-image-container'>
-            <img src={single.url} />
-            <h1>{single.title}</h1>
-            <p>{single.description}</p>
-            <p>{single.dateTaken}</p>
+            <img src={single?.url} />
+            <h1>{single?.title}</h1>
+            <p>{single?.description}</p>
+            <p>{single?.dateTaken}</p>
 
         </div>
     )
